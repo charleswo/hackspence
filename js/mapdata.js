44ee89393b2eb987ea3936b5b9b1dd20d791e41b
charleswo <charleswolloch@yahoo.com>
2015-04-25 13:09:37 -0400

    d

--- js/mapdata.js
+++ js/mapdata.js
@@ -9,37 +9,26 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-
+var shelters = ([{lat:"",long:"",name:"",disp:""},{lat:"",long:"",name:"",disp:""},{lat:"",long:"",name:"",disp:""}]);
 
 function make_marker(lat,long,name,disp){
   L.marker([lat, long]).addTo(map)
     .bindPopup("<b>" + name + "</b><br />" + disp).openPopup();
   }
 
-  var lat = ["40.77947229999999","40.7653925"];
-  var long = ["-73.95479219999999","-73.98903860000001"];
-  var name = ["Neighborhood Coalition-Shelter","Covenant House"];
-  var disp = ["157 E 86th St, (212) 249-3595","427 W 52nd St (212) 247-5670"];
-
-make_marker(40.77947229999999, -73.95479219999999, "Neighborhood Coalition-Shelter", "157 E 86th St, (212) 249-3595");
 
     L.marker([40.7840468, -73.9570915]).addTo(map)
             .bindPopup("<b>current location</b><br />you are here.").openPopup();
 
+    L.marker([40.77947229999999, -73.95479219999999]).addTo(map)
+            .bindPopup("<b>Neighborhood Coalition-Shelter</b><br >157 E 86th St<br> (212) 249-3595").openPopup();
 
 
+    L.marker([40.7653925, -73.98903860000001]).addTo(map)
+            .bindPopup("<b>Covenant House</b><br > 427 W 52nd St<br/> (212) 247-5670").openPopup();
 
     L.marker([40.7629981, -73.99221299999999]).addTo(map)
             .bindPopup("<b> Safe Haven</b><br >448 W 48th St<br> (212) 246-8321").openPopup();
-
-    L.marker([40.723658, -73.984127]).addTo(map)
-                    .bindPopup("<b> Nazareth Housing</b><br >206 E 4th St<br> (212) 477-7017").openPopup();
-
-    L.marker([40.7175359, -74.00162109999997]).addTo(map)
-                            .bindPopup("<b> New York City Rescue Mission</b><br >90 Lafayette St<br> (212) 226-6214").openPopup();
-
-    L.marker([40.7483775, -73.9897302]).addTo(map)
-                                                    .bindPopup("<b> Women In Need Inc</b><br >115 W 31st St<br> (212) 695-4758").openPopup();
 
 L.circle([40.7840468, -73.9570915], 500, {
     color: 'red',
