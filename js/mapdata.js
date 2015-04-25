809937ce112c36500c661938077d700a0dfec4c0
charleswo <charleswolloch@yahoo.com>
2015-04-25 13:29:48 -0400

    d

--- js/mapdata.js
+++ js/mapdata.js
@@ -13,34 +13,11 @@ function make_marker(lat,long,name,disp1,disp2){
   L.marker([lat, long]).addTo(map)
     .bindPopup("<b>" + name + "</b><br />" + disp1 + "<br>" + disp2 + "</br>").openPopup();
   }
-
-<<<<<<< HEAD
-
-    L.marker([40.7840468, -73.9570915]).addTo(map)
-            .bindPopup("<b>current location</b><br />you are here.").openPopup();
-
-    L.marker([40.77947229999999, -73.95479219999999]).addTo(map)
-            .bindPopup("<b>Neighborhood Coalition-Shelter</b><br >157 E 86th St<br> (212) 249-3595").openPopup();
-
-
-    L.marker([40.7653925, -73.98903860000001]).addTo(map)
-            .bindPopup("<b>Covenant House</b><br > 427 W 52nd St<br/> (212) 247-5670").openPopup();
-
-    L.marker([40.7629981, -73.99221299999999]).addTo(map)
-            .bindPopup("<b> Safe Haven</b><br >448 W 48th St<br> (212) 246-8321").openPopup();
-
-            L.marker([40.723658, -73.984127]).addTo(map)
-                    .bindPopup("<b> Nazareth Housing</b><br >206 E 4th St<br> (212) 477-7017").openPopup();
-
-    L.marker([40.7175359, -74.00162109999997]).addTo(map)
-                            .bindPopup("<b> New York City Rescue Mission</b><br >90 Lafayette St<br> (212) 226-6214").openPopup();
-
-    L.marker([40.7483775, -73.9897302]).addTo(map)
-                            .bindPopup("<b> Women In Need Inc</b><br >115 W 31st St<br> (212) 695-4758").openPopup();
-
     L.marker([40.786508, -73.97072400000002]).addTo(map)
                             .bindPopup("<b>Dorot</b><br >44 W 87th St<br> (212) 580-0001").openPopup();
-=======
+
+
+
     make_marker(40.7840468, -73.9570915, "current location", "you are", "here");
     make_marker(40.77947229999999, -73.95479219999999, "Neighborhood Coalition-Shelter", "157 E 86th St", "(212) 249-3595");
     make_marker(40.7653925, -73.98903860000001, "Covenant House", "427 W 52nd St","(212) 247-5670");
@@ -48,7 +25,7 @@ function make_marker(lat,long,name,disp1,disp2){
     make_marker(40.723658, -73.984127, "Nazareth Housing", "206 E 4th St","(212) 477-7017");
     make_marker(40.7175359, -74.00162109999997, "New York City Rescue Mission", "90 Lafayette St","(212) 226-6214");
     make_marker(40.7483775, -73.9897302, "Women In Need Inc", "115 W 31st St","(212) 695-4758");
->>>>>>> origin/master
+
 
 L.circle([40.7840468, -73.9570915], 500, {
     color: 'red',
